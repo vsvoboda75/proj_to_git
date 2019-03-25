bc99160120f163b5185a5d73a5f26410f5d2fb76
vsvoboda75 <48914139+vsvoboda75@users.noreply.github.com>
2019-03-25 17:29:53 +0100

    second commit

--- src/com/company/NewClass.java
+++ src/com/company/NewClass.java
@@ -1,1 +1,10 @@
+package com.company;
 
+public class NewClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello world2!");
+    }
+}
